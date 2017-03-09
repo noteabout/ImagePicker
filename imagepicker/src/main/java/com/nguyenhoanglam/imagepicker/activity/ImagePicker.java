06b423e74101e6982f631a92243b5aff019b5107
noteabout <noteaboutinc@gmail.com>
2017-03-09 19:58:29 +0800

    Update ImagePicker.java

--- imagepicker/src/main/java/com/nguyenhoanglam/imagepicker/activity/ImagePicker.java
+++ imagepicker/src/main/java/com/nguyenhoanglam/imagepicker/activity/ImagePicker.java
@@ -22,6 +22,7 @@ public abstract class ImagePicker {
 
     private int mode;
     private int limit;
+    private boolean showImages;
     private boolean showCamera;
     private String folderTitle;
     private String imageTitle;
@@ -67,6 +68,7 @@ public abstract class ImagePicker {
     public void init(Activity activity) {
         this.mode = ImagePickerActivity.MODE_MULTIPLE;
         this.limit = Constants.MAX_LIMIT;
+        this.showImages = true;
         this.showCamera = true;
         this.folderTitle = activity.getString(R.string.title_folder);
         this.imageTitle = activity.getString(R.string.title_select_image);
@@ -130,6 +132,11 @@ public abstract class ImagePicker {
         return this;
     }
 
+    public ImagePicker showImages(boolean showImages) {
+        this.showImages = showImages;
+        return this;
+    }
+
     public Intent getIntent(Activity activity) {
         Intent intent = new Intent(activity, ImagePickerActivity.class);
         intent.putExtra(ImagePickerActivity.INTENT_EXTRA_MODE, mode);
@@ -139,6 +146,7 @@ public abstract class ImagePicker {
         intent.putExtra(ImagePickerActivity.INTENT_EXTRA_IMAGE_TITLE, imageTitle);
         intent.putExtra(ImagePickerActivity.INTENT_EXTRA_SELECTED_IMAGES, selectedImages);
         intent.putExtra(ImagePickerActivity.INTENT_EXTRA_FOLDER_MODE, folderMode);
+        intent.putExtra(ImagePickerActivity.INTENT_EXTRA_SHOW_IMAGES, showImages);
         intent.putExtra(ImagePickerActivity.INTENT_EXTRA_IMAGE_DIRECTORY, imageDirectory);
         return intent;
     }
